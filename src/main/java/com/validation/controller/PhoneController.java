8009d33786c6f620fbafeb12c68b109e318f1c82
tuan anh <tuananhtran.xd@gmail.com>
2024-03-06 14:00:26 +0700

    first commit

---
+++ src/main/java/com/validation/controller/PhoneController.java
@@ -0,0 +1,32 @@
+package com.validation.controller;
+
+import com.validation.model.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("phone")
+public class PhoneController {
+    @GetMapping("")
+    public String showForm(Model model){
+        model.addAttribute("phoneNumber", new PhoneNumber());
+        return "index";
+    }
+    @PostMapping("check")
+    public String checkValidation(@Valid @ModelAttribute("phoneNumber") PhoneNumber phoneNumber, BindingResult bindingResult, Model model){
+        new PhoneNumber().validate(phoneNumber, bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "index";
+        } else {
+            model.addAttribute("phoneNumber",phoneNumber);
+            return "/result";
+        }
+    }
+}
